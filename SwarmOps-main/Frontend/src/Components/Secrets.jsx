8efc96bab6a1fc0e59774c55d063f0ad40113a77
Jai Pagare <jai.pagare@realpage.com>
2024-10-10 11:11:07 +0530

    Jk First commit

---
+++ SwarmOps-main/Frontend/src/Components/Secrets.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+
+const Secrets = () => {
+  return (
+    <>
+      <nav class="bg-blue-400">
+        <span class="ml-8 text-3xl font-bold text-white">Secrets</span>
+      </nav>
+      <div class="flex flex-row space-x-5 ">
+        <div class="w-fit bg-white: rounded-xl shadow-md p-8 mt-8 hover:shadow-lg  hover:bg-gray-100 ml-8">
+          <div class=" flex flex-col">
+            <h2 class="text-xl font-bold">Name of the Node</h2>
+            <p class="text-gray-600">
+              Id: <span class="text-gray-900">gdhsacsdwi234</span>
+            </p>
+          </div>
+          <br />
+          <div class="flex  flex-row space-x-2">
+            <button class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
+              Inspect
+            </button>
+            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+              Update
+            </button>
+            <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+              Delete
+            </button>
+          </div>
+        </div>
+      
+      <div>
+        <div class="w-fit bg-white: rounded-xl shadow-md p-8 mt-8 hover:shadow-lg  hover:bg-gray-100 ml-8 ">
+          <div class=" flex flex-col">
+            <h2 class="text-xl font-bold">Name of the Node</h2>
+            <p class="text-gray-600">
+              Id: <span class="text-gray-900">gdhsacsdwi234</span>
+            </p>
+          </div>
+          <br />
+          <div class="flex  flex-row space-x-2">
+            <button class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
+              Inspect
+            </button>
+            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+              Update
+            </button>
+            <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+              Delete
+            </button>
+          </div>
+        </div>
+      </div>
+      </div>
+    </>
+  )
+}
+
+export default Secrets
