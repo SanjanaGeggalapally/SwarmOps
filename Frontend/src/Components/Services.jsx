08a858cff342a0f8f6e2dcb5e34a594709fba9e7
JkPagare <jaikumar16723@gmail.com>
2024-10-25 14:59:24 +0530

    My Last Commit <3

--- Frontend/src/Components/Services.jsx
+++ Frontend/src/Components/Services.jsx
@@ -93,7 +93,7 @@ useEffect(() => {
             />
           </div>
         </div>
-        <div className="overflow-x-auto overflow-y-auto">
+        <div className="overflow-x-auto overflow-y-auto max-h-96">
         <table class=" min-w-full  border border-gray-300 text-sm text-left text-gray-500 dark:text-gray-400">
           <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
